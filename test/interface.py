e35711c85c2dbef8f2c019d959efa03f3fdba219
Anna ZOU <89054199+AnnaZou1103@users.noreply.github.com>
2023-03-09 13:44:01 +0800

    feat(test): add user interface

---
+++ test/interface.py
@@ -0,0 +1,104 @@
+import tkinter as tk
+import tkinter.filedialog
+import time
+import os
+from process_file import process_file
+
+
+def set_image():
+    entry_upload_path['state'] = 'normal'
+    btn_upload['state'] = 'normal'
+
+
+def set_video():
+    entry_upload_path['state'] = 'normal'
+    btn_upload['state'] = 'normal'
+
+
+def set_upload_path():
+    path_ = tkinter.filedialog.askopenfilename()
+    upload_path.set(path_)
+
+
+def select_save_path():
+    path_ = tkinter.filedialog.askdirectory()
+    save_path.set(path_ + '/')
+
+
+def process():
+    model_path = 'checkpoints/pretrain/best.pth'
+
+    start = time.time()
+    process_file(model_path, upload_path.get(), save_path.get(), input_type.get())
+    end = time.time()
+    seconds = end - start
+
+    hour = int(seconds / 3600)
+    minute = int(seconds % 3600 / 60)
+    second = int(seconds % 60)
+
+    topw, toph = 310, 100
+    ctpx = int(sw / 2 - topw / 2)
+    ctpy = int(sh / 2 - toph / 2)
+    top = tk.Toplevel(window)
+    top.geometry(f"{topw}x{toph}+{ctpx}+{ctpy}")
+    top.title('Notice')
+    finish_top = tk.Label(top, font=('Arial', 14),
+                          text='Process completed in ' + str(hour) + 'h ' + str(minute) + "m " + str(second) + 's. ')
+    finish_top.place(x=10, y=10)
+    btn_ok = tk.Button(top, font=('Arial', 14), text="OK", width=8, command=top.destroy)
+    btn_ok.place(x=100, y=50)
+    entry_upload_path.delete(0, 'end')
+
+
+if __name__ == '__main__':
+    window = tk.Tk()
+    window.title('Facial Emotion Recognition')
+
+    sw = window.winfo_screenwidth()
+    sh = window.winfo_screenheight()
+    ww = 650
+    wh = 210
+    x = int((sw - ww) / 2)
+    y = int((sh - wh) / 2)
+    window.geometry(f"{ww}x{wh}+{x}+{y}")
+
+    input_type = tk.StringVar()
+    label_input = tk.Label(window, font=('Arial', 14), text='Please select the input type:')
+    label_input.place(x=10, y=10)
+
+    input_type.set('Image')
+    r_btn_input_1 = tk.Radiobutton(window, font=('Arial', 14), text='Image', variable=input_type, value='Image',
+                                   command=set_image)
+    r_btn_input_1.place(x=200, y=10)
+
+    r_btn_input_2 = tk.Radiobutton(window, font=('Arial', 14), text='Video', variable=input_type, value='Video',
+                                   command=set_video)
+    r_btn_input_2.place(x=350, y=10)
+
+    upload_path = tk.StringVar()
+    label_upload = tk.Label(window, text="Input Path:", font=('Arial', 14))
+    label_upload.place(x=10, y=60)
+    entry_upload_path = tk.Entry(window, font=('Arial', 14), textvariable=upload_path)
+    entry_upload_path.place(x=120, y=60, width=350)
+    btn_upload = tk.Button(window, text="Open", font=('Arial', 14), width=10, command=set_upload_path)
+    btn_upload.place(x=480, y=60)
+
+    default_save_path = 'output/processed_media/'
+    tk.Label(window, text='Save Path:', font=('Arial', 14)).place(x=10, y=110)
+    if not os.path.exists(default_save_path):
+        os.makedirs(default_save_path)
+    save_path = tk.StringVar()
+    save_path.set(default_save_path)
+    entry_save_path = tk.Entry(window, font=('Arial', 14), textvariable=save_path)
+    entry_save_path.place(x=120, y=110, width=350)
+    btn_save = tk.Button(window, text="Open", font=('Arial', 14), width=10, command=select_save_path)
+    btn_save.place(x=480, y=110)
+
+    btn_process = tk.Button(window, font=('Arial', 14), text="Execute", width=10, command=process)
+    btn_process.place(x=180, y=160)
+
+    btn_quit = tk.Button(window, font=('Arial', 14), text="Quit", width=10, command=window.quit)
+    btn_quit.place(x=340, y=160)
+
+    window.mainloop()
--- test/process_file.py
+++ test/process_file.py
@@ -2,12 +2,15 @@ import torch
 import torch.nn as nn
 from retinaface import RetinaFace
 import torchvision.transforms as transforms
+from timm.models import swinv2_base_window16_256
 from torch.autograd import Variable
 import cv2
 from PIL import Image
 import time
 import numpy as np
 import math
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def findEuclideanDistance(source_representation, test_representation):
@@ -62,7 +65,7 @@ def alignment_procedure(img, left_eye, right_eye, nose):
     return img
 
 
-def fer(img):
+def fer(img, model):
     emotion = ['surprise', 'fear', 'disgust', 'happiness', 'sadness', 'anger', 'neutral']
     transform_test = transforms.Compose([
         transforms.Resize((256, 256)),
@@ -80,61 +83,60 @@ def fer(img):
     return emotion[pred.data.item()]
 
 
-if __name__ == '__main__':
-    DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = torch.load('../checkpoints/pretrain/best.pth')
+def detect_face(image, model):
+    results = RetinaFace.detect_faces(image)
+    if type(results) == dict:
+        for key in results:
+            identity = results[key]
+            facial_area = identity["facial_area"]
+            if facial_area[3] - facial_area[1] <= 10 or facial_area[2] - facial_area[0] <= 10:
+                continue
+            else:
+                facial_img = image[facial_area[1]: facial_area[3], facial_area[0]: facial_area[2]]
+                w = facial_area[2] - facial_area[0]
+                h = facial_area[3] - facial_area[1]
+
+                landmarks = identity["landmarks"]
+                left_eye = landmarks["left_eye"]
+                right_eye = landmarks["right_eye"]
+                nose = landmarks["nose"]
+                facial_img = alignment_procedure(facial_img, right_eye, left_eye, nose)
+
+                label = fer(Image.fromarray(facial_img), model)
+                cv2.rectangle(image, (facial_area[0], facial_area[1]),
+                              (facial_area[0] + w, facial_area[1] + h), (36, 255, 12), 1)
+                cv2.putText(image, label, (facial_area[0], facial_area[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                            (36, 255, 12), 2)
+    return image
+
+
+def process_file(model_path, file_path, save_path, type='Video'):
+    model = torch.load(model_path)
 
     model.eval()
     model.to(DEVICE)
 
-    video_path = '../dataset/video.mp4'
-    save_path = '../output/'
-    capture = cv2.VideoCapture(video_path)
+    capture = cv2.VideoCapture(file_path)
     frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
     frame_rate = capture.get(cv2.CAP_PROP_FPS)
     frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    out = cv2.VideoWriter(save_path + video_path.split('/')[-1], cv2.VideoWriter_fourcc(*'mp4v'), frame_rate,
-                          (frame_width, frame_height))
 
     start = time.time()
-    while capture.isOpened():
-        rval, frame = capture.read()
-        if rval:
-            results = RetinaFace.detect_faces(frame)
-            image = frame
-            if type(results) == dict:
-                for key in results:
-                    identity = results[key]
-                    facial_area = identity["facial_area"]
-                    if facial_area[3] - facial_area[1] <= 10 or facial_area[2] - facial_area[0] <= 10:
-                        continue
-                    else:
-                        facial_img = frame[facial_area[1]: facial_area[3], facial_area[0]: facial_area[2]]
-                        w = facial_area[2] - facial_area[0]
-                        h = facial_area[3] - facial_area[1]
-
-                        landmarks = identity["landmarks"]
-                        left_eye = landmarks["left_eye"]
-                        right_eye = landmarks["right_eye"]
-                        nose = landmarks["nose"]
-                        mouth_right = landmarks["mouth_right"]
-                        mouth_left = landmarks["mouth_left"]
-                        facial_img = alignment_procedure(facial_img, right_eye, left_eye, nose)
-
-                        label = fer(Image.fromarray(facial_img))
-                        cv2.rectangle(image, (facial_area[0], facial_area[1]),
-                                      (facial_area[0] + w, facial_area[1] + h), (36, 255, 12), 1)
-                        cv2.putText(image, label, (facial_area[0], facial_area[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
-                                    (36, 255, 12), 2)
-            out.write(image)
-            # cv2.imshow("Frame", image)
-            # key = cv2.waitKey(1) & 0xFF
-            # # quit
-            # if key == ord("q"):
-            #     break
-        else:
-            break
+    if type == 'Video':
+        out = cv2.VideoWriter(save_path + file_path.split('/')[-1], cv2.VideoWriter_fourcc(*'mp4v'), frame_rate,
+                              (frame_width, frame_height))
+        while capture.isOpened():
+            rval, frame = capture.read()
+            if rval:
+                frame = detect_face(frame, model)
+                out.write(frame)
+            else:
+                break
+    elif type == 'Image':
+        img = cv2.imread(file_path)
+        img = detect_face(img, model)
+        cv2.imwrite(save_path + file_path.split('/')[-1], img)
 
     end = time.time()
     seconds = end - start
